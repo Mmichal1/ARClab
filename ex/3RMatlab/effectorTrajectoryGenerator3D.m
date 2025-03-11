8c8c496c8b35646de8126471f71e7eca57291a5b
Michal Trela <michal.trela@arendai.com>
2025-03-11 16:10:27 +0100

    solution for lab1

--- ex/3RMatlab/effectorTrajectoryGenerator3D.m
+++ ex/3RMatlab/effectorTrajectoryGenerator3D.m
@@ -12,8 +12,18 @@ function [ qchd, qchd_d1, qchd_d2 ] = effectorTrajectoryGenerator3D( t, paramete
     qchd = [k*cos(w*t)+dx; k*sin(w*t)+dy; 0.1*k*cos(w*t)+dz];
     
     %caclulate first and second derivative based on the qchd vector
-    qchd_d1 = [0; 0; 0];
-    qchd_d2 = [0; 0; 0];
+    qchd_d1 = [ ...
+        -k*w*sin(w*t);
+        k*w*cos(w*t);
+        -0.1*k*w*sin(w*t)
+    ];
+
+    % second derivatives
+    qchd_d2 = [ ...
+        -k*w^2*cos(w*t);
+        -k*w^2*sin(w*t);
+        -0.1*k*w^2*cos(w*t)
+    ];
     
 end
 
--- ex/3RMatlab/modelODE.m
+++ ex/3RMatlab/modelODE.m
@@ -69,25 +69,27 @@ function [ output_args, additional ] = modelODE( t, input_args, parameters )
     %calculate friction in manipulator joints
     %use Tustin model with Tv, Ts and Tk coefficients
     %velocities for each joints are in vector q_d1
+    T = ...
+    [[Tv * q_d1(1) + Ts * sign(q_d1(1))]; [Tv * q_d1(2) + Ts * sign(q_d1(2))]; [Tv * q_d1(3) + Ts * sign(q_d1(3))]];
 
     %generate trajectory for end effector    
     %remember to calculate 1st and 2nd derivative of desired end-effector
     %trajectory
     [qchd, qchd_d1, qchd_d2] = effectorTrajectoryGenerator3D(t, parameters);
 
-        MInv = M^-1;
+        % MInv = M^-1;
 
     %     y'' = P - J M^-1 C q' + J M^-1 u
 
-        detJ = det(J);
+        %detJ = det(J);
 
         % calculate F
         % F = P - J M^-1 C q' - J M^-1 D - J M^-1 T        
-        F = zeros(3,1);
+        % F = zeros(3,1);
         
         %calculate G
         % G = J M^-1
-        G = zeros(3,3);
+        % G = zeros(3,3);
 
         qch = [xch; ych; zch];
         qch_d1 = [xch_d1; ych_d1; zch_d1];
@@ -96,12 +98,12 @@ function [ output_args, additional ] = modelODE( t, input_args, parameters )
         % for e' keep e_d1 notation
         % e = qch - qchd;
         % e' = qch' - qchd';
-        e = zeros(3,1);
-        e_d1 = zeros(3,1);
+        % e = zeros(3,1);
+        % e_d1 = zeros(3,1);
 
         %calculate new input to the system v
         % v = qd'' - Kd e' - Kp e
-        v = zeros(3,1);
+        % v = zeros(3,1);
 
         detG = det(G);
         % calculate inverse of G as below
@@ -109,10 +111,28 @@ function [ output_args, additional ] = modelODE( t, input_args, parameters )
 
         % calculate control input u
         % u = G^-1 * (v - F)
-        u = zeros(3,1);
+        % u = zeros(3,1);
         
+
+
+
+
+        F = P ...
+            - J*(MInv*C*q_d1) ...
+            - J*(MInv*D) ...
+            - J*(MInv*T);
+
+        G = J*inv(M)*B;
+
+        e    = qch - qchd;
+        e_d1 = qch_d1 - qchd_d1;
+
+        v = qchd_d2 - Kd*e_d1 - Kp*e;
+
+        u = inv(G)*( v - F );
+
         % calculate state
-        qr_d2 = MInv * (B * u - C * q_d1 - D - T);   
+        qr_d2 = inv(M) * (B * u - C * q_d1 - D - T);   
 
     dets = [detG; detJ];            
         
